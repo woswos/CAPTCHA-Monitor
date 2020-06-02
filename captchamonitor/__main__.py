4674e771dee5600c75b561fb4f95f2c794da58a6
Barkin Simsek <barkin@nyu.edu>
2020-06-02 16:34:37 +0000

    added the setup file

--- captchamonitor/__main__.py
+++ captchamonitor/__main__.py
@@ -1,16 +1,23 @@
 from captchamonitor.chef import CaptchaMonitor
 import logging
 import time
+import argparse
 
 logger_format = '%(asctime)s %(module)s [%(levelname)s] %(message)s'
 logging.basicConfig(format=logger_format)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-if __name__ == '__main__':
-
-    config_file = '/home/woswos/CAPTCHA-Monitor/captchamonitor/resources/config.ini'
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', dest='config_file', help='Absolute path to the configuration file', required=True)
+    args = parser.parse_args()
+    config_file = args.config_file
 
     while True:
         CaptchaMonitor.run(config_file)
         time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='CAPTCHA Monitor',
+      version='0.1',
+      description='Check if a web site returns a CAPTCHA',
+      url='https://github.com/woswos/CAPTCHA-Monitor',
+      author='Barkin Simsek',
+      author_email='barkin@nyu.edu',
+      license='GPL-3.0',
+      packages=['captchamonitor'],
+      install_requires=[
+            'selenium-wire',
+            'urltools'
+      ],
+      zip_safe=False,
+      entry_points={
+            'console_scripts': [
+            'captchamonitor = captchamonitor.__main__:main'
+            ]
+      })
