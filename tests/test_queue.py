2821155913611a363593dc4a32eb048c7b2b93c2
Barkin Simsek <barkin@nyu.edu>
2020-06-08 17:50:09 +0000

    add queue test

---
+++ tests/test_queue.py
@@ -0,0 +1,73 @@
+import pytest
+import os
+from captchamonitor.utils.queue import Queue
+import sqlite3
+import random
+import string
+
+
+def randomString(stringLength=10):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+db_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cm.db')
+
+job_1 = {'method': randomString(),
+         'url': randomString(),
+         'captcha_sign': randomString(),
+         'additional_headers': randomString(),
+         'exit_node': randomString(),
+         'tbb_security_level': randomString()}
+
+job_2 = {'method': randomString(),
+         'url': randomString(),
+         'captcha_sign': randomString(),
+         'additional_headers': randomString(),
+         'exit_node': randomString(),
+         'tbb_security_level': randomString()}
+
+
+@pytest.fixture
+def fresh_queue():
+    # Remove the existing test db
+    if os.path.exists(db_file):
+        os.remove(db_file)
+
+    # Create a new test queue
+    queue = Queue(db_file)
+    return queue
+
+
+def test_queue_creation(fresh_queue):
+    assert fresh_queue.get_job() == None
+
+
+def test_queue_add_job(fresh_queue):
+    fresh_queue.add_job(job_1)
+    job = fresh_queue.get_job()
+    assert job['url'] == job_1['url']
+
+
+def test_queue_remove_job_with_single_job(fresh_queue):
+    fresh_queue.add_job(job_1)
+
+    # Remove the job
+    job = fresh_queue.get_job()
+    fresh_queue.remove_job(job['id'])
+
+    assert fresh_queue.get_job() == None
+
+
+def test_queue_remove_job_with_multiple_job(fresh_queue):
+    fresh_queue.add_job(job_1)
+    fresh_queue.add_job(job_2)
+
+    # Remove the first job
+    job = fresh_queue.get_job()
+    fresh_queue.remove_job(job['id'])
+
+    # Get the remaining jobs, which should be job 2
+    job = fresh_queue.get_job()
+
+    assert job['url'] == job_2['url']
