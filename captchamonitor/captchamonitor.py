ab477edc651cd4e76d5a2690d1e64dd118a02ab2
Barkin Simsek <barkin@nyu.edu>
2020-06-09 00:09:50 +0000

    revised cli

--- captchamonitor/captchamonitor.py
+++ captchamonitor/captchamonitor.py
@@ -1,8 +1,4 @@
-from captchamonitor.chef import CaptchaMonitor
-from captchamonitor.utils.db_export import export
-from captchamonitor.utils.queue import Queue
 import logging
-import time
 import argparse
 import sys
 import os
@@ -12,101 +8,250 @@ logging.basicConfig(format=logger_format)
 logger = logging.getLogger('captchamonitor')
 logger.setLevel(logging.INFO)
 
+env_vars = {'CM_TBB_PATH': 'The path to Tor Browser bundle',
+            'CM_TOR_HOST': 'The IP address of the Tor server',
+            'CM_TOR_PORT': 'The port number of the Tor server',
+            'CM_DB_FILE_PATH': 'The path to the database file'}
+
+for env_var in env_vars:
+    if env_var not in os.environ:
+        logger.warning('%s (%s) is not set in the environment' % (env_vars[env_var], env_var))
+        sys.exit()
+
+
+MAIN_DESC = """
+CAPTCHA Monitor
+"""
+
+MAIN_HELP = """
+Use 'captchamonitor <subcommand> --help' for more info
+"""
+
+ADD_JOB_DESC = 'Add a new job to queue'
+ADD_JOB_HELP = 'Add a new job to queue'
+
+RUN_DESC = 'Run jobs in the queue'
+RUN_HELP = 'Run jobs in the queue'
+
+EXPORT_DESC = 'Export the database tables to JSON files into a specified directory'
+EXPORT_HELP = 'Export the database tables to JSON files'
+
+CLOUDFLARE_DESC = 'Change Cloudflare security levels'
+CLOUDFLARE_HELP = 'Change Cloudflare security levels'
+
+
+def formatter_class(prog):
+    return argparse.HelpFormatter(prog, max_help_position=52)
+
 
 class main():
     def __init__(self):
-        self.default_config_file_path = os.path.join(os.getcwd(), 'config.ini')
-        parser = argparse.ArgumentParser(description='CAPTCHA Monitor',
-                                         usage='''captchamonitor <command> [<args>]
+        main_parser = argparse.ArgumentParser(description=MAIN_DESC)
 
-Available commands:
-run     Run the CAPTCHA Monitor in loop until stopped by the user
-export  Export the captured data to a JSON file
-add     Add a new job to the database
-                                        ''')
-        parser.add_argument('command', help='Subcommand to run')
-        args = parser.parse_args(sys.argv[1:2])
-        if not hasattr(self, args.command):
-            print('Unrecognized command')
-            parser.print_help()
-            sys.exit()
-        # Run the corresponding command
-        getattr(self, args.command)()
+        sub_parser = main_parser.add_subparsers(help=MAIN_HELP)
 
-    def add(self):
-        if os.path.isfile(self.default_config_file_path) and os.access(self.default_config_file_path, os.R_OK):
-            config_file = self.default_config_file_path
-            parser = argparse.ArgumentParser()
-            parser.add_argument('-u', '--url')
-            parser.add_argument('-m', '--method')
-            parser.add_argument('-c', '--captcha_sign')
-            parser.add_argument('-a', '--additional_headers')
-            parser.add_argument('-e', '--exit_node')
-            parser.add_argument('-s', '--tbb_security_level')
-            args = parser.parse_args(sys.argv[2:])
-            logger.info('Using the configuration file found in the current working directory')
+        ###########
+        # ADD JOB #
+        ###########
+        add_job_parser = sub_parser.add_parser('add',
+                                               description=ADD_JOB_DESC,
+                                               help=ADD_JOB_HELP,
+                                               formatter_class=formatter_class)
 
+        add_job_parser.set_defaults(func=self.add_job, formatter_class=formatter_class)
+
+        add_job_parser.add_argument('-u', '--url',
+                                    help="""the interesting URL to fetch""",
+                                    metavar='URL',
+                                    required='True',
+                                    default='')
+        add_job_parser.add_argument('-m', '--method',
+                                    help="""the webrowser/tool to fetch the provided URL""",
+                                    metavar='NAME',
+                                    required='True',
+                                    default='')
+        add_job_parser.add_argument('-c', '--captcha_sign',
+                                    help="""the text that will be searched for""",
+                                    metavar='TEXT',
+                                    required='True',
+                                    default='')
+        add_job_parser.add_argument('-a', '--additional_headers',
+                                    help="""specify additional headers for the job""",
+                                    metavar='HEADER',
+                                    default='')
+        add_job_parser.add_argument('-e', '--exit_node',
+                                    help="""specify the Tor exit node (if fetching over Tor)""",
+                                    metavar='IP',
+                                    default='')
+        add_job_parser.add_argument('-s', '--tbb_security_level',
+                                    help="""specify the Tor Browser security level (if using Tor Browser)""",
+                                    metavar="LEVEL",
+                                    default='medium')
+
+        #######
+        # RUN #
+        #######
+        run_parser = sub_parser.add_parser('run',
+                                           description=RUN_DESC,
+                                           help=RUN_HELP,
+                                           formatter_class=formatter_class)
+
+        run_parser.set_defaults(func=self.run, formatter_class=formatter_class)
+
+        run_parser.add_argument('-l', '--loop',
+                                help="""use this argument to process jobs in a loop""",
+                                action='store_true')
+
+        ##########
+        # EXPORT #
+        ##########
+        export_parser = sub_parser.add_parser('export',
+                                              description=EXPORT_DESC,
+                                              help=EXPORT_HELP,
+                                              formatter_class=formatter_class)
+
+        export_parser.set_defaults(func=self.export, formatter_class=formatter_class)
+
+        export_parser.add_argument('-p', '--path',
+                                   help="""use this argument to process jobs in a loop""",
+                                   required='True',
+                                   metavar="PATH")
+
+        ##############
+        # CLOUDFLARE #
+        ##############
+        cloudflare_parser = sub_parser.add_parser('cloudflare_change',
+                                                  description=CLOUDFLARE_DESC,
+                                                  help=CLOUDFLARE_HELP,
+                                                  formatter_class=formatter_class)
+
+        cloudflare_parser.set_defaults(func=self.cloudflare_change, formatter_class=formatter_class)
+
+        cloudflare_parser.add_argument('-e', '--email',
+                                       help="""your Cloudflare email""",
+                                       required='True',
+                                       metavar="EMAIL")
+
+        cloudflare_parser.add_argument('-t', '--token',
+                                       help="""your Cloudflare API token""",
+                                       required='True',
+                                       metavar="TOKEN")
+
+        cloudflare_parser.add_argument('-d', '--domain',
+                                       help="""use this argument to process jobs in a loop""",
+                                       required='True',
+                                       metavar="NAME")
+
+        cloudflare_parser.add_argument('-s', '--security_level',
+                                       help="""use this argument to process jobs in a loop""",
+                                       required='True',
+                                       metavar="NAME")
+
+        # Get args and call the command handler for the chosen mode
+        if len(sys.argv) == 1:
+            main_parser.print_help()
+            sys.exit(1)
         else:
-            parser = argparse.ArgumentParser()
-            parser.add_argument('config_file')
-            parser.add_argument('-u', '--url')
-            parser.add_argument('-m', '--method')
-            parser.add_argument('-c', '--captcha_sign')
-            parser.add_argument('-a', '--additional_headers')
-            parser.add_argument('-e', '--exit_node')
-            parser.add_argument('-s', '--tbb_security_level')
-            args = parser.parse_args(sys.argv[2:])
-            config_file = args.config_file
-            logger.info('Using the given configuration file')
+            args = main_parser.parse_args()
+            args.func(args)
 
-        queue = Queue(config_file)
-        queue.add_job(args.method, args.url, args.captcha_sign, args.additional_headers, args.exit_node, args.tbb_security_level)
+    def add_job(self, args):
+        """
+        Add a new job to the queue
+        """
+        from captchamonitor.utils.queue import Queue
+
+        data = {'method': args.method,
+                'url': args.url,
+                'captcha_sign': args.captcha_sign,
+                'additional_headers': args.additional_headers,
+                'exit_node': args.exit_node,
+                'tbb_security_level': args.tbb_security_level}
+
+        queue = Queue()
+        queue.add_job(data)
 
         sys.exit()
 
-    def run(self):
-        if os.path.isfile(self.default_config_file_path) and os.access(self.default_config_file_path, os.R_OK):
-            config_file = self.default_config_file_path
-            logger.info('Using the configuration file found in the current working directory')
-        else:
-            parser = argparse.ArgumentParser(description='Path to the configuration file')
-            parser.add_argument('config_file')
-            args = parser.parse_args(sys.argv[2:])
-            config_file = args.config_file
-            logger.info('Using the given configuration file')
+    def run(self, args):
+        from captchamonitor.utils.queue import Queue
+        from captchamonitor.utils.fetch import fetch_via_method
+        from captchamonitor.utils.captcha import detect
+        from captchamonitor.utils.sqlite import SQLite
+        import time
+
+        loop = args.loop
 
         try:
-            logger.info('Started running CAPTCHA Monitor in the continous mode')
+            if loop:
+                logger.info('Started running CAPTCHA Monitor in the continous mode')
+            else:
+                logger.info('Started running CAPTCHA Monitor')
+
+            queue = Queue()
+            db = SQLite()
+
             while True:
-                CaptchaMonitor.run(config_file)
+                job_details = queue.get_job()
+
+                # Process the job if there is one in the queue
+                if job_details is not None:
+                    # Fetch the URL using the method specified
+                    fetched_data = fetch_via_method(job_details)
+
+                    # Detect any CAPTCHAs
+                    fetched_data['is_captcha_found'] = detect(
+                        job_details['captcha_sign'], fetched_data['html_data'])
+
+                    # Delete columns that we don't want in the results table
+                    del job_details['id']
+                    del job_details['additional_headers']
+
+                    # Combine the fetched data and job details
+                    results = {**job_details, **fetched_data}
+
+                    # Insert into the database
+                    db.insert_result(results)
+
+                elif not loop:
+                    logger.info('No job found in the queue, exitting...')
+                    sys.exit()
+
                 time.sleep(1)
 
         except KeyboardInterrupt:
             logger.info('Stopping, bye!')
             sys.exit()
 
-    def export(self):
-        if os.path.isfile(self.default_config_file_path) and os.access(self.default_config_file_path, os.R_OK):
-            config_file = self.default_config_file_path
-            logger.info('Using the configuration file found in the current working directory')
-        else:
-            parser = argparse.ArgumentParser(description='Path to the configuration file')
-            parser.add_argument('config_file')
-            args = parser.parse_args(sys.argv[2:])
-            config_file = args.config_file
-            logger.info('Using the given configuration file')
+    def export(self, args):
+        from captchamonitor.utils.db_export import export
+
+        export_location = args.path
 
         try:
-            logger.info('Exporting...')
-            export(config_file)
+            logger.info('Exporting to %s', export_location)
+            export(export_location)
             logger.info('Done!')
-            sys.exit()
 
         except KeyboardInterrupt:
             logger.info('Stopping, bye!')
-            sys.exit()
+
+        sys.exit()
+
+    def cloudflare_change(self, args):
+        from captchamonitor.utils.cloudflare import Cloudflare
+
+        email = args.email
+        api_token = args.token
+        domain = args.domain
+        security_level = args.security_level
+
+        cloudflare = Cloudflare(email, api_token)
+        zone_ids = cloudflare.get_zone_ids()
+        cloudflare.set_zone_security_level(zone_ids[domain], security_level)
+
+        sys.exit()
 
 
 if __name__ == '__main__':
-    print('test')
     main()
