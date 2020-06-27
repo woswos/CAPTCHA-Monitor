e5dd9935ff4c67bcbe6f1c6a8e80352215c5043d
Barkin Simsek <barkin@nyu.edu>
2020-06-27 23:57:00 +0000

    add compose submodule template

---
+++ captchamonitor/compose.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import os
+import logging
+from captchamonitor.utils.queue import Queue
+import sys
+
+
+def compose(args):
+    """
+    Automatically add new jobs base on the job history
+    """
+    logger = logging.getLogger(__name__)
+
+
+    sys.exit()
