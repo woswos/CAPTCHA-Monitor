3381703ea858945677ad943924f81538e491d65c
Barkin Simsek <barkin@nyu.edu>
2020-07-15 12:41:00 +0000

    updated chromium fetcher to use HTTP Header Live extension

--- captchamonitor/fetchers/chromium.py
+++ captchamonitor/fetchers/chromium.py
@@ -1,13 +1,16 @@
 """
-Fetch a given URL using seleniumwire and Chromium
+Fetch a given URL using selenium and Chromium
 """
 
+import os
+import pathlib
+import glob
 import logging
 import json
-import sys
 import socket
-from urltools import compare
-from seleniumwire import webdriver
+import time
+from pyvirtualdisplay import Display
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import captchamonitor.utils.format_requests as format_requests
 
@@ -15,46 +18,127 @@ import captchamonitor.utils.format_requests as format_requests
 def fetch_via_chromium(url, additional_headers=None, timeout=30, **kwargs):
     logger = logging.getLogger(__name__)
 
+    try:
+        download_folder = os.environ['CM_DOWNLOAD_FOLDER']
+    except Exception as err:
+        logger.error('Some of the environment variables are missing: %s', err)
+
     results = {}
 
-    # Choose the headless mode
+    chrome_driver_path = '/snap/bin/chromium.chromedriver'
+    http_header_live_extension_id = 'mhmbgecfengpllohfhnkpapmbgiphhff'
+
+    # Find the right extension
+    http_header_live_folder = '../assests/http_header_live/'
+    script_path = pathlib.Path(__file__).parent.absolute()
+    search_string = os.path.abspath(os.path.join(script_path,
+                                                 http_header_live_folder,
+                                                 '*.crx'))
+    http_header_live_extension = glob.glob(search_string)[0]
+
+    http_header_live_export_file = os.path.join(download_folder,
+                                                'captcha_monitor_website_data.json')
+
+    # Delete the previous HTTP-Header-Live export
+    if os.path.exists(http_header_live_export_file):
+        os.remove(http_header_live_export_file)
+
+    # Start virtual display because Chromium doesn't support extensions in headless mode
+    display = Display(visible=True, size=(1000, 900), backend='xvfb')
+    display.start()
+
     options = Options()
-    options.headless = True
-    options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
+    #options.headless = True
+    # options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+
+    # Set the download folder and disable pop up windows
+    prefs = {"profile.default_content_settings.popups": 0,
+             "download.default_directory": download_folder,
+             "directory_upgrade": True}
+    options.add_experimental_option("prefs", prefs)
+
+    # Required to enabled third party extensions
+    options.add_argument('–-enable-easy-off-store-extension-install')
+    options.add_extension(http_header_live_extension)
 
     # Set the timeout for webdriver initialization
-    #socket.setdefaulttimeout(15)
+    # socket.setdefaulttimeout(15)
 
     try:
-        driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome(options=options,
+                                  executable_path=chrome_driver_path)
     except Exception as err:
+        display.stop()
         logger.error('Couldn\'t initialize the browser, check if there is enough memory available: %s'
                      % err)
         return None
 
-    if additional_headers:
-        driver.header_overrides = json.loads(additional_headers)
+    # Start the HTTP-Header-Live extension and switch to a new tab for the actual URL fetch
+    try:
+        driver.get('chrome-extension://%s/HTTPHeaderMain.html' % http_header_live_extension_id)
+        driver.execute_script("window.open('about:blank', 'tab2');")
+        driver.switch_to.window("tab2")
+
+    except Exception as err:
+        driver.quit()
+        display.stop()
+        logger.error('Couldn\'t launch HTTP-Header-Live: %s' % err)
+        return None
 
     # Set driver page load timeout
     driver.implicitly_wait(timeout)
-    #socket.setdefaulttimeout(timeout)
+    # socket.setdefaulttimeout(timeout)
 
     # Try sending a request to the server and get server's response
     try:
         driver.get(url)
 
+        # Chromium returns a valid HTML page even if the page was not fetched.
+        #   The returned page contains the license and 'The Chromium Authors'
+        #   was choosen because of this reason.
+        if 'The Chromium Authors' in driver.page_source:
+            raise Exception('This site can’t be reached')
+
     except Exception as err:
         driver.quit()
+        display.stop()
         logger.error('webdriver.Chrome.get() says: %s' % err)
+        return None
+
+    # Wait for HTTP-Header-Live extension to finish
+    timeout = 20
+    requests_data = None
+    logger.debug('Waiting for HTTP-Header-Live extension')
+    for counter in range(timeout):
+        try:
+            with open(http_header_live_export_file) as file:
+                requests_data = json.load(file)
+                break
+
+        except OSError:
+            time.sleep(1)
+
+        except Exception as err:
+            driver.quit()
+            logger.error('Cannot parse the headers: %s' % err)
+            return None
+
+    if requests_data is None:
+        driver.quit()
+        # Don't return anything since we couldn't capture the headers
+        logger.error('Couldn\'t capture the headers from %s' % http_header_live_export_file)
         return None
 
     # Record the results
     results['html_data'] = driver.page_source
-    results['requests'] = format_requests.seleniumwire(driver.requests)
+    results['requests'] = format_requests.tb(requests_data, url)
 
     logger.debug('I\'m done fetching %s', url)
 
     driver.quit()
+    display.stop()
 
     return results
