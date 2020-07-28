4bb48125e0eee9b7e7a9ff7d4da007fe3aab2270
woswos <barkin@nyu.edu>
2020-07-28 13:21:31 +0000

    bump version to 0.2.1

--- captchamonitor/__init__.py
+++ captchamonitor/__init__.py
@@ -9,5 +9,5 @@ from .worker import worker as worker
 from .run import run as run
 
 __author__ = 'Barkin Simsek'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __version_info__ = tuple([ int(num) for num in __version__.split('.')])
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     install_requires = f.readlines()
 
 setup(name='CAPTCHA Monitor',
-      version='0.2.0',
+      version='0.2.1',
       description='Check if a web site returns a CAPTCHA',
       url='https://github.com/woswos/CAPTCHA-Monitor',
       author='Barkin Simsek',
