7cdbf68f835e9608261380e75494c33461ebc313
Barkin Simsek <barkin@nyu.edu>
2021-04-06 14:16:07 +0000

    Make internal methods and attributes private in Tor Launcher

--- src/captchamonitor/utils/tor_launcher.py
+++ src/captchamonitor/utils/tor_launcher.py
@@ -19,20 +19,25 @@ class TorLauncher:
         """
         Initialize Tor Launcher
         """
-        self.logger = logging.getLogger(__name__)
-        self.config = config
+        # Public class attributes
+        self.ip_address = None
+        self.socks_port = None
+        self.control_port = None
+
+        self.__logger = logging.getLogger(__name__)
+        self.__config = config
 
         try:
-            self.docker_network_name = self.config["docker_network"]
+            self.__docker_network_name = self.__config["docker_network"]
 
         except Exception as exception:
-            self.logger.warning("Could not connect to Tor:\n %s", exception)
+            self.__logger.warning("Could not connect to Tor:\n %s", exception)
             raise TorLauncherInitError from exception
 
-        self.launch_tor_container()
-        self.bind_stem_to_tor_container()
+        self.__launch_tor_container()
+        self.__bind_stem_to_tor_container()
 
-    def launch_tor_container(self):
+    def __launch_tor_container(self):
         """
         Launches a new Tor container with the given configuration
 
@@ -42,14 +47,16 @@ class TorLauncher:
         client = docker.from_env()
 
         # Find the CAPTCHA Monitor network
-        networks = client.networks.list(names=[self.docker_network_name])
+        networks = client.networks.list(names=[self.__docker_network_name])
 
         if len(networks) == 0:
-            self.logger.warning("Could not find the Docker network for CAPTCHA Monitor")
+            self.__logger.warning(
+                "Could not find the Docker network for CAPTCHA Monitor"
+            )
             raise TorLauncherInitError
 
         if len(networks) != 1:
-            self.logger.warning(
+            self.__logger.warning(
                 "There are multiple Docker networks for CAPTCHA Monitor, I don't know which one to choose"
             )
             raise TorLauncherInitError
@@ -57,11 +64,10 @@ class TorLauncher:
         # Find unused ports
         self.socks_port = str(port_for.select_random())
         self.control_port = str(port_for.select_random())
-        self.tor_dir = "/var/lib/tor"
 
         # Initialize a new Tor container
-        self.container = client.containers.run(
-            image=self.config["docker_tor_container_image"],
+        self.__container = client.containers.run(
+            image=self.__config["docker_tor_container_image"],
             command=[
                 "/usr/bin/tor",
                 "--SocksPort",
@@ -69,8 +75,8 @@ class TorLauncher:
                 "--ControlPort",
                 f"0.0.0.0:{self.control_port}",
                 "--HashedControlPassword",
-                str(self.config["docker_tor_authentication_password_hashed"]),
-                # f"--DataDirectory {self.tor_dir}",
+                str(self.__config["docker_tor_authentication_password_hashed"]),
+                # "--DataDirectory '/var/lib/tor'",
                 # "--CookieAuthentication 1",
                 # "--PathsNeededToBuildCircuits 0.95",
                 # "--LearnCircuitBuildTimeout 10",
@@ -91,62 +97,61 @@ class TorLauncher:
         )
 
         # Reload container attributes
-        self.container.reload()
+        self.__container.reload()
         # Now obtain the IP address
-        container_networks = self.container.attrs["NetworkSettings"]["Networks"]
+        container_networks = self.__container.attrs["NetworkSettings"][
+            "Networks"
+        ]
         self.ip_address = str(list(container_networks.values())[0]["IPAddress"])
 
-        self.logger.debug(
+        self.__logger.debug(
             "Initialized new Tor container at %s with %s as socks port and %s as control port",
             self.ip_address,
             self.socks_port,
             self.control_port,
         )
 
-    def bind_stem_to_tor_container(self):
+    def __bind_stem_to_tor_container(self):
         """
         Binds Tor Stem to the Tor Container launched earlier
         """
-
-        # Make sure the container was actually intialized
-        assert hasattr(self, "container")
-        assert hasattr(self, "socks_port")
-        assert hasattr(self, "control_port")
-        assert hasattr(self, "tor_dir")
-        assert hasattr(self, "ip_address")
-
-        self.tor_password = self.config["docker_tor_authentication_password"]
+        self.__tor_password = self.__config[
+            "docker_tor_authentication_password"
+        ]
 
         # Try connecting 3 times
+        connected = False
         for _ in range(3):
             try:
-                self.controller = Controller.from_port(
+                self.__controller = Controller.from_port(
                     address=str(self.ip_address), port=int(self.control_port)
                 )
-                self.controller.authenticate(password=self.tor_password)
+                self.__controller.authenticate(password=self.__tor_password)
+                connected = True
                 break
+
             except stem.SocketError as exception:
-                self.logger.debug(
+                self.__logger.debug(
                     "Unable to connect to the Tor Container, retrying: %s",
                     exception,
                 )
                 time.sleep(3)
 
-        # Check if database connection was made
-        if not hasattr(self, "controller"):
-            self.logger.warning(
+        # Check if connection was successfull
+        if not connected:
+            self.__logger.warning(
                 "Could not connect to the Tor Container after many retries"
             )
             raise StemConnectionInitError
 
-        self.logger.info(
+        self.__logger.info(
             "Connected to the Tor Container, the Tor version running on the container is %s",
-            self.controller.get_version(),
+            self.__controller.get_version(),
         )
 
     def __del__(self):
         # Close connection
-        self.controller.close()
+        self.__controller.close()
 
         # Kill the container
-        self.container.kill()
+        self.__container.kill()
