885e3872342f1e5ac366f75bb4fc77d40f51740a
Barkin Simsek <barkin@nyu.edu>
2021-05-20 21:56:59 +0000

    Update exception descriptions

--- src/captchamonitor/utils/exceptions.py
+++ src/captchamonitor/utils/exceptions.py
@@ -6,54 +6,54 @@ class Error(Exception):
 
 class DatabaseInitError(Error):
     def __str__(self):
-        return "Database initialization error"
+        return "DatabaseInitError: Database initialization error"
 
 
 class ConfigInitError(Error):
     def __str__(self):
-        return "Configuration initialization error"
+        return "ConfigInitError: Configuration initialization error"
 
 
 class TorLauncherInitError(Error):
     def __str__(self):
-        return "Tor Launcher initialization error"
+        return "TorLauncherInitError: Tor Launcher initialization error"
 
 
 class StemConnectionInitError(Error):
     def __str__(self):
-        return "Stem cannot connect to the Tor container"
+        return "StemConnectionInitError: Stem cannot connect to the Tor container"
 
 
 class StemDescriptorUnavailableError(Error):
     def __str__(self):
-        return "Stem cannot get relay descriptors"
+        return "StemDescriptorUnavailableError: Stem cannot get relay descriptors"
 
 
 class WorkerInitError(Error):
     def __str__(self):
-        return "Worker initialization error"
+        return "WorkerInitError: Worker initialization error"
 
 
 class HarExportExtensionXpiError(Error):
     def __str__(self):
-        return "Provided Har Export Trigger extension is not valid"
+        return "HarExportExtensionXpiError: Provided Har Export Trigger extension is not valid"
 
 
 class FetcherConnectionInitError(Error):
     def __str__(self):
-        return "Fetcher wasn't initialized as expected"
+        return "FetcherConnectionInitError: Fetcher wasn't initialized as expected"
 
 
 class FetcherURLFetchError(Error):
     def __str__(self):
-        return "Fetcher wasn't able to provided URL"
+        return "FetcherURLFetchError: Fetcher wasn't able to process the provided URL"
 
 
 class TorBrowserProfileLocationError(Error):
     def __str__(self):
-        return "The provided location is inaccessible or not a valid directory"
+        return "TorBrowserProfileLocationError: The provided location is inaccessible or not a valid directory"
 
 
 class FetcherNotFound(Error):
     def __str__(self):
-        return "The provided fetcher method is not available"
+        return "FetcherNotFound: The provided fetcher method is not available"
