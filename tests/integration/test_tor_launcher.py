ad43a845c21159fcbe08a085a6135f27c91b3899
Barkin Simsek <barkin@nyu.edu>
2021-05-31 17:16:21 +0000

    Add tests for Tor Launcher

---
+++ tests/integration/test_tor_launcher.py
@@ -0,0 +1,27 @@
+import pytest
+import unittest
+from captchamonitor.utils.tor_launcher import TorLauncher
+from captchamonitor.utils.config import Config
+from captchamonitor.utils.small_scripts import deep_copy
+from captchamonitor.utils.exceptions import TorLauncherInitError
+
+
+class TestTorLauncher(unittest.TestCase):
+    def setUp(self):
+        self.config = Config()
+
+    def test_tor_launcher_init(self):
+        TorLauncher(self.config)
+
+    def test_wrong_network_name(self):
+        test_config = deep_copy(self.config)
+
+        # Modify the network name
+        test_config["docker_network"] = "obviously_wrong_network"
+
+        # Try intializing
+        with pytest.raises(TorLauncherInitError) as pytest_wrapped_e:
+            TorLauncher(test_config)
+
+        # Check if the exception is correct
+        self.assertEqual(pytest_wrapped_e.type, TorLauncherInitError)
