fc317448c006fc51c57792e97df215bb9c578dba
Barkin Simsek <barkin@nyu.edu>
2021-06-23 15:02:36 +0000

    Let workers activate GDPR remover based on exit relay location

--- src/captchamonitor/core/worker.py
+++ src/captchamonitor/core/worker.py
@@ -1,14 +1,20 @@
 import sys
+import json
 import time
 import logging
 import traceback
-from typing import Union, Optional
+from typing import List, Union, Optional
 
 from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker
 
 from captchamonitor.utils.config import Config
-from captchamonitor.utils.models import FetchQueue, FetchFailed, FetchCompleted
+from captchamonitor.utils.models import (
+    MetaData,
+    FetchQueue,
+    FetchFailed,
+    FetchCompleted,
+)
 from captchamonitor.utils.exceptions import FetcherNotFound
 from captchamonitor.utils.tor_launcher import TorLauncher
 from captchamonitor.fetchers.tor_browser import TorBrowser
@@ -51,12 +57,50 @@ class Worker:
         self.__worker_id: str = worker_id
         self.__tor_launcher: TorLauncher = TorLauncher(self.__config)
         self.__job_queue_delay: float = float(self.__config["job_queue_delay"])
+        self.__gdpr_keywords: List[str]
         self.__fetcher: Union[TorBrowser, FirefoxBrowser, ChromeBrowser, OperaBrowser]
+
+        self.__get_gdpr_keywords()
 
         # Loop over the jobs
         while loop:
             self.process_next_job()
             time.sleep(self.__job_queue_delay)
+
+    def __get_gdpr_keywords(self) -> None:
+        metadata_key = "gdpr_keywords"
+        default_keywords = [
+            "Souhlasím",
+            "Alle akzeptieren",
+            "Jag godkänner",
+            "Ich stimme zu",
+            "Ik ga akkoord",
+            "Godta alle",
+            "Egyetértek",
+            "J'accepte",
+            "I agree",
+            "Acceptai tot",
+            "Accept all",
+            "Accept",
+        ]
+
+        query = self.__db_session.query(MetaData).filter(MetaData.key == metadata_key)
+
+        # Check if the keywords exist in the database
+        if query.count() == 0:
+            # Create a new keyword list if it doesn't exist
+            metadata = MetaData(
+                key=metadata_key,
+                value=json.dumps(default_keywords),
+            )
+            self.__db_session.add(metadata)
+            self.__db_session.commit()
+
+            self.__gdpr_keywords = default_keywords
+
+        else:
+            # Get the values from the db
+            self.__gdpr_keywords = json.loads(query.one().value)
 
     def process_next_job(self) -> None:
         """
@@ -68,7 +112,7 @@ class Worker:
 
         :raises FetcherNotFound: If requested fetcher is not available
         """
-        # pylint: disable=R0912
+        # pylint: disable=R0912,R0915
         # Get claimed jobs by this worker
         db_job = self.__db_session.query(FetchQueue).filter(
             FetchQueue.claimed_by == self.__worker_id
@@ -91,15 +135,30 @@ class Worker:
             return
 
         try:
+            # Create the options based on the ones described within the job
+            if job.options is not None:
+                options_dict = job.options
+            else:
+                options_dict = {}
+
             # Create a new circuit if we will be using Tor
             if job.ref_fetcher.uses_tor is True:
                 self.__tor_launcher.create_new_circuit_to(job.ref_relay.fingerprint)
 
+            # If we need to use an exit relay and if the exit relay is located
+            # in Europe, enable GDPR removing functionality
+            if job.ref_fetcher.uses_tor:
+                if job.ref_relay.continent in ("Europe", None):
+                    options_dict.update(
+                        {
+                            "gdpr_remove": True,
+                            "gdpr_keywords": self.__gdpr_keywords,
+                        }
+                    )
+
             # Fetch it using a fetcher
             if job.ref_fetcher.method == TorBrowser.method_name_in_db:
-                options_dict = {"tbb_security_level": job.tbb_security_level}
-                if job.options is not None:
-                    options_dict.update(job.options)
+                options_dict.update({"tbb_security_level": job.tbb_security_level})
                 self.__fetcher = TorBrowser(
                     config=self.__config,
                     url=job.url,
@@ -113,7 +172,7 @@ class Worker:
                     config=self.__config,
                     url=job.url,
                     tor_launcher=self.__tor_launcher,
-                    options=job.options,
+                    options=options_dict,
                     use_tor=job.ref_fetcher.uses_tor,
                 )
 
@@ -122,7 +181,7 @@ class Worker:
                     config=self.__config,
                     url=job.url,
                     tor_launcher=self.__tor_launcher,
-                    options=job.options,
+                    options=options_dict,
                     use_tor=job.ref_fetcher.uses_tor,
                 )
 
@@ -131,7 +190,7 @@ class Worker:
                     config=self.__config,
                     url=job.url,
                     tor_launcher=self.__tor_launcher,
-                    options=job.options,
+                    options=options_dict,
                     use_tor=job.ref_fetcher.uses_tor,
                 )
 
@@ -151,7 +210,7 @@ class Worker:
             # If failed, put into the failed table
             failed = FetchFailed(
                 url=job.url,
-                options=job.options,
+                options=options_dict,
                 tbb_security_level=job.tbb_security_level,
                 captcha_monitor_version=self.__config["version"],
                 fail_reason=str(error),
@@ -180,7 +239,7 @@ class Worker:
             # If successful, put into the completed table
             completed = FetchCompleted(
                 url=job.url,
-                options=job.options,
+                options=options_dict,
                 tbb_security_level=job.tbb_security_level,
                 captcha_monitor_version=self.__config["version"],
                 html_data=self.__fetcher.page_source,
